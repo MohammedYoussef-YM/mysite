975cffc76a9f20950eaaea121726d63def632a1b
YoussefAlsodi <83075331+MohammedYoussef-YM@users.noreply.github.com>
2024-09-02 19:30:01 +0300

    Add files via upload

---
+++ ilearnsite/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.decorators import login_required
+# from my_app.views import my_protected_view
+urlpatterns = [
+
+    path('set-language/', views.set_language, name='set_language'),
+    path("", views.index, name="index"),
+    path("customers", views.customers, name="customers"),
+    path("belongtous", views.members, name="belongtous"),
+    path("community", views.community, name="community"),  
+    path("about", views.about, name="about"),
+    path("ourServices", views.ourServices, name="ourServices"),
+    path("hasad", views.hasad, name="hasad"),
+    path("locationBarnch", views.locationBarnch, name="locationBarnch"),
+    path("ourNew", views.ourNew, name="ourNew"),
+    path("404", views.errorpage, name="404"),
+  ]
+   
+
